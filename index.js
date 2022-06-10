c307aa23bc71ebea8ed6225ccfdb522996ec2ad7
Merkie <merkie@post.com>
2022-06-10 14:19:07 -0500

    changes

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
+// Imports
 const express = require("express");
+const https = require("https");
 const mongoose = require("mongoose");
 const secret = require("./secret");
-const port = 3000;
+const fs = require('fs');
 
+// Schemas
 const User = require("./schema/User");
 const Layout = require("./schema/Layout");
 
+// Express app
 const app = express();
+const port = 3000;
 
-app.listen(port, () => {
-	console.log("Server has started!");
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
+
+const sslServer = https.createServer(
+	{
+		key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+		cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+	},
+	app
+);
+
+sslServer.listen(port, () => console.log('Secure server 🚀🔑 on port 3443'))
 
 mongoose.connect(secret.mongourl).then(() => {
 	console.log("Connected to MongoDB!");
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+
 
 const new_layout = new Layout({
 	name: "My First Layout",
@@ -39,16 +52,3 @@ async function run() {
 	console.log("\n");
 	console.log(new_user);
 }
-
-//run();
-
-/*
-app.get("/", (req, res) => {
-  console.log(req.headers);
-  res.send("Hello World, from express");
-});
-
-app.listen(port, () =>
-  console.log(`Hello world app listening on port ${port}!`)
-);
- */
