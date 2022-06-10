5b279542a94814822e7aa692a52f3dcd41fab9d0
Merkie <merkie@post.com>
2022-06-10 15:05:00 -0500

    signup

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function hashPassword(password) {
 	
     
 	
-    var hash = crypto.pbkdf2(password, salt, iterations, 64, 'sha512').toString('base64');
+    var hash = crypto.pbkdf2(password, salt, iterations, 64, 'sha512', (err) => console.log(err)).toString('base64');
 
     // return {
     //     salt: salt,
